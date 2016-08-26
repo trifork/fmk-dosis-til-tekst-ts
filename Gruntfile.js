d3f56a0ade4c420d3ab9b6869b6ac9fb3a743a8a
Claus Hemberg Jørgensen <chj@trifork.com>
2016-08-26 07:24:36 +0200

    npm-publish stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,55 @@
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   var webpackConfig = require("./webpack.config.js");
-  
+
   grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 
-      clean: [ 'target' ],
+    clean: ['target'],
 
-      tslint: {
-            options: {
-                configuration: "tslint.json"
-            },
-            files: [
-                "src/main/ts/**/*.ts",
-                "src/main/ts/**/*.tsx"
-            ]
-        },
+    tslint: {
+      options: {
+        configuration: "tslint.json"
+      },
+      files: [
+        "src/main/ts/**/*.ts",
+        "src/main/ts/**/*.tsx"
+      ]
+    },
 
-      webpack: {
-        dosistiltekst: webpackConfig 
+    'npm-publish': {
+      options: {
+        abortIfDirty: false
       }
-    });
+    },
+
+    webpack: {
+      dosistiltekst: webpackConfig
+    }
+  });
 
   // Load all grunt tasks
   require('load-grunt-tasks')(grunt);
   require('webpack');
 
-/*
-  grunt.registerTask('default', 'Builds the project.', function() {
-      const watch = grunt.option('watch');
-      const webpack = grunt.config.get('webpack');
-      if (watch && webpack) {
-          Object.keys(webpack).forEach((entry) => {
-              webpack[entry].watch = true;
-              webpack[entry].keepalive = true;
-              webpack[entry].failOnError = false;
-          });
-          grunt.config.set('webpack', webpack);
-      }
-      grunt.task.run('clean', 'tslint', 'webpack');
-    });*/
-    grunt.registerTask('default', ['clean', 'tslint', 'webpack']);
-    
-    grunt.registerTask('publish', ['default', 'npm-publish']);
-    
+  /*
+    grunt.registerTask('default', 'Builds the project.', function() {
+        const watch = grunt.option('watch');
+        const webpack = grunt.config.get('webpack');
+        if (watch && webpack) {
+            Object.keys(webpack).forEach((entry) => {
+                webpack[entry].watch = true;
+                webpack[entry].keepalive = true;
+                webpack[entry].failOnError = false;
+            });
+            grunt.config.set('webpack', webpack);
+        }
+        grunt.task.run('clean', 'tslint', 'webpack');
+      });*/
+  grunt.registerTask('default', ['clean', 'tslint', 'webpack']);
+
+  grunt.registerTask('publish', ['default', 'npm-publish']);
+
 }   //new GruntConfiguration(); 
--- README.md
+++ README.md
@@ -5,16 +5,16 @@ To build the project:
 Windows:
 1) Install nodejs (http://nodejs.org) and npm
 2) 'npm install -g grunt-cli'
-2) 'npm install'
-3) 'grunt'
+3) 'npm install'
+4) 'grunt'
 
 Linux:
-1) sudo add-apt-repository ppa:chris-lea/node.js  (standard distro grunt doesn't work :-) )
+1) sudo add-apt-repository ppa:chris-lea/node.js  (standard distro grunt doesn't work :-( )
 2) sudo apt-get update 
 3) sudo apt-get install nodejs 
 4) sudo npm install -g grunt-cli 
 
 Mac:
-...who really cares :-)
+...who cares :-)
 
 The typescript code is compiled and webpack'ed into target/dosistiltekst.js
