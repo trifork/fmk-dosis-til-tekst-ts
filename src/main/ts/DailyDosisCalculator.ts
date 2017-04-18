37f5672f194529e68d8b004c4bd479aea487c875
Claus Hemberg Jørgensen <chj@trifork.com>
2017-04-18 16:08:14 +0200

    Bugfix: dailydosis blev ikke afrundet til 9 dec. som xml-skemaet kræver

--- src/main/ts/DailyDosisCalculator.ts
+++ src/main/ts/DailyDosisCalculator.ts
@@ -74,8 +74,8 @@ export class DailyDosisCalculator {
     private static calculateAvg(sum: Interval<number>, divisor: number, unitOrUnits: UnitOrUnitsWrapper): DailyDosis {
 
         let avg: Interval<number> = {
-            minimum: sum.minimum / divisor,
-            maximum: sum.maximum / divisor
+            minimum: parseFloat((sum.minimum / divisor).toPrecision(9)),
+            maximum: parseFloat((sum.maximum / divisor).toPrecision(9))
         };
 
         if (avg.maximum - avg.minimum < 0.000000001)
