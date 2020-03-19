b53bf794abf11417d5a5138808752ef7498b67e9
Claus Hemberg Jørgensen <chj@trifork.com>
2020-03-19 17:05:35 +0100

    FMK-5873 del 2
    (cherry picked from commit 88b39722fbd5237968918259a0d47e67d020b7ab)

--- src/main/ts/shorttextconverterimpl/SimpleLimitedAccordingToNeedConverterImpl.ts
+++ src/main/ts/shorttextconverterimpl/SimpleLimitedAccordingToNeedConverterImpl.ts
@@ -41,7 +41,7 @@ export class SimpleLimitedAccordingToNeedConverterImpl extends ShortTextConverte
         text += " efter behov";
 
         if (day.getNumberOfAccordingToNeedDoses() === 1)
-            text += ", højst 1 gang daglig";
+            text += ", højst 1 gang dagligt";
         else
             text += ", højst " + day.getNumberOfAccordingToNeedDoses() + " gange daglig";
 
